29780b7700dd8fbb1752d3c135ecde4b5432e4f7
Alex Lange <alex@imalange.com>
2025-02-17 17:32:52 -0600

    testing

--- src/Components/CodeEditor.jsx
+++ src/Components/CodeEditor.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Controlled as ControlledEditor } from 'react-codemirror2';
 import 'codemirror/lib/codemirror.css'; // For the code editor styles
 import 'codemirror/mode/javascript/javascript'; // JavaScript mode for syntax highlighting
+import 'codemirror/mode/htmlmixed/htmlmixed.js'; // HTML mode for syntax highlighting
+import 'codemirror/mode/css/css.js'; // CSS mode for syntax highlighting
 
 export const CodeEditor = ({ code, setCode, disabled,language}) => {
     return (
